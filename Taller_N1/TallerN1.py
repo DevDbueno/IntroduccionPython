a277ff8065612aba71ef47a97a670848ca21b6ca
Diego A Bueno <104845463+DevDbueno@users.noreply.github.com>
2023-09-09 12:19:52 -0500

    Actividad N°1
    Primeras actividades:
    Imprimir en consola los dias de la semana y meses del año

---
+++ Taller_N1/TallerN1.py
@@ -0,0 +1,10 @@
+Semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+Meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+print("Dias de la semana: ")
+for i in Semana:
+    print(i)
+
+print("Meses del año: ")
+for l in Meses:
+    print(l)
